870d4b0f831ee0abcffb60df0d38a5b9d6304256
Joohyung Lee <something1712@gmail.com>
2021-03-01 18:25:08 -0600

    functional

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [numbersArray, setNumbersArray] = React.useState([1, 2, 3, 4, 5]);
+
+	const handleOnDragEnd = (result, arrayToRearrange) => {
+		console.log(result);
+		if (!result.destination) {
+			return;
+		}
+
+		let rearrangedArray = [];
+
+		const copyOfArray = [...arrayToRearrange];
+
+		let draggedElement = copyOfArray[result.source.index];
+
+		copyOfArray.forEach((number, index) => {
+			if (index === result.source.index) {
+				console.log(number);
+			} else if (index === result.destination.index) {
+				result.destination.index > result.source.index
+					? rearrangedArray.push(number, draggedElement)
+					: rearrangedArray.push(draggedElement, number);
+			} else {
+				rearrangedArray.push(number);
+			}
+		});
+
+		setNumbersArray(rearrangedArray);
+	};
+
+	return (
+		<DragDropContext
+			onDragEnd={(result) => handleOnDragEnd(result, numbersArray)}
+		>
+			{/* TODO: droppableId allows the library to keep track of this specific instance between interactions */}
+			<Droppable droppableId="numbers">
+				{/* TODO: this provided argument includes information and references to code that the library needs to work properly */}
+				{(provided) => (
+					<ul
+						className="app_numbers-container"
+						{...provided.droppableProps}
+						ref={provided.innerRef}
+					>
+						{numbersArray.map((number, index) => {
+							return (
+								<Draggable
+									key={`number_${index}`}
+									draggableId={`draggable-number_${index}`}
+									index={index}
+								>
+									{(provided) => (
+										<li
+											className="app_number"
+											ref={provided.innerRef}
+											{...provided.draggableProps}
+											{...provided.dragHandleProps}
+										>
+											{number}
+										</li>
+									)}
+								</Draggable>
+							);
+						})}
+						{provided.placeholder}
+					</ul>
+				)}
+			</Droppable>
+		</DragDropContext>
+	);
 }
 
 export default App;
